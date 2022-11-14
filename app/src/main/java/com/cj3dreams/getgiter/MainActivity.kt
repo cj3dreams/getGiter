c08fb33b2885851689b861d236aa27e919382aef
3Dimensional <cj3dream@gmail.com>
2022-11-14 16:31:05 +0500

    First commit
    -Project was made
    -Added dependencies

---
+++ app/src/main/java/com/cj3dreams/getgiter/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cj3dreams.getgiter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/cj3dreams/getgiter/source/remote/GithubApiRequest.kt
@@ -0,0 +1,4 @@
+package com.cj3dreams.getgiter.source.remote
+
+interface GithubApiRequest {
+}
